c1d76798b49884f655c0dfafe633ae7c17d7fadc
unknown <cik789@naver.com>
2026-02-02 17:27:57 +0900

    feat: 학습공간 Url 수정 2차

--- backend/src/main/java/com/teamlms/backend/domain/study_rental/api/StudySpaceController.java
+++ backend/src/main/java/com/teamlms/backend/domain/study_rental/api/StudySpaceController.java
@@ -56,7 +56,7 @@ public class StudySpaceController {
 
     // 학습공간 상세 조회
     @GetMapping({ "/api/v1/student/spaces/{spaceId}",
-            "api/v1/admin/spaces/{spaceId}" })
+            "/api/v1/admin/spaces/{spaceId}" })
     @PreAuthorize("hasAuthority('SPACE_READ')")
     public ApiResponse<SpaceDetailResponse> getSpaceDetail(@PathVariable Long spaceId) {
         return ApiResponse.ok(queryService.getSpaceDetail(spaceId));
